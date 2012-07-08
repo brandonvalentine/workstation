ef3e301f982ba5a6c05c7f1b26a560c36a4826d2
Brandon Valentine <brandon@brandonvalentine.com>
2012-07-08 02:32:41 -0500

    Remove irrelevant recipes

--- attributes/rubymine.rb
+++
@@ -1,6 +0,0 @@
-node.default["rubymine"]= {
-    :major => "4",
-    :minor => "0",
-    :micro => "3",
-    :pivotal_preferences_hash => "b0cd41aa211c612eb8295dd66a632cdb5689d0a3"
-}
--- recipes/libreoffice.rb
+++
@@ -1,9 +0,0 @@
-node.default["libreoffice"]["source"] = "http://download.documentfoundation.org/libreoffice/stable/3.5.4/mac/x86/LibO_3.5.4_MacOS_x86_install_en-US.dmg"
-node.default["libreoffice"]["checksum"] = "b4302f30f6aafe384f13673aaba05ff85c1b4617f997c22e1079df48c6172fee"
-
-pivotal_workstation_package "LibreOffice" do
-  volumes_dir "LibreOffice"
-  source node["libreoffice"]["source"]
-  checksum node["libreoffice"]["checksum"]
-  action :install
-end
