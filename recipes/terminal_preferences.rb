dbf4facb97b82d863c292f4ac2232e7bd0570603
Brandon Valentine <brandon@brandonvalentine.com>
2012-07-23 16:52:08 -0500

    Add default terminal color scheme and preferences

--- recipes/terminal_preferences.rb
+++ recipes/terminal_preferences.rb
@@ -1,4 +1,22 @@
-execute "set terminal color scheme" do
-  command "defaults write com.apple.Terminal 'Startup Window Settings' -string #{node['terminal']['color_scheme']}"
+gem_package("osx-plist")
+
+terminal_plist_file = "#{ENV['HOME']}/Library/Preferences/com.apple.Terminal.plist"
+color_scheme_plist_file = "#{File.dirname(__FILE__)}/../files/default/4Dwm.terminal"
+
+require 'osx/plist'
+
+plist = OSX::PropertyList.load_file(terminal_plist_file)
+color_scheme = OSX::PropertyList.load_file(color_scheme_plist_file)
+
+plist["Window Settings"]["4Dwm"] = color_scheme
+OSX::PropertyList.dump_file(terminal_plist_file, plist, :binary1)
+
+execute "set startup terminal color scheme" do
+  command "defaults write com.apple.Terminal 'Startup Window Settings' -string 4Dwm"
+  user WS_USER
+end
+
+execute "set default terminal color scheme" do
+  command "defaults write com.apple.Terminal 'Default Window Settings' -string 4Dwm"
   user WS_USER
 end
