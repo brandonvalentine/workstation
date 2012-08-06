61860a561238e0fae26ca08f183d1b298dba0089
Brandon Valentine <brandon@brandonvalentine.com>
2012-08-06 12:58:43 -0500

    Add recipe to install Carbon Copy Cloner

---
+++ recipes/carbon_copy_cloner.rb
@@ -0,0 +1,5 @@
+workstation_package "Carbon Copy Cloner" do
+  source "http://www.bombich.com/software/download_ccc.php?v=3.5.1"
+  action :install
+  checksum "83204723cc39e2d0ca5d000cdd0a00a05d17edd6cc529ece3e89e3ea8dc4e79e"
+end
