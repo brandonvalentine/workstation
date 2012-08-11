3e85fb91a76a0ad08689172729cb4b91472f7fc0
Brandon Valentine <brandon@brandonvalentine.com>
2012-08-11 15:48:04 -0500

    Autocreate N_PREFIX dir

--- recipes/node_js.rb
+++ recipes/node_js.rb
@@ -8,3 +8,9 @@ execute "install n version manager for node.js" do
 end
 
 workstation_bash_profile_include "node_js"
+
+execute "make directory to hold installations of node via n version manager" do
+  command "mkdir #{ENV['HOME']}/.n"
+  not_if File.directory? "#{ENV['HOME']}/.n"
+  user WS_USER
+end
