235423e493881af8c16ed80a61427e6ab58fe33a
Brandon Valentine <brandon@brandonvalentine.com>
2012-08-27 17:22:15 -0500

    Remove n version manager from node recipe. It blows.

--- recipes/node_js.rb
+++ recipes/node_js.rb
@@ -1,16 +1,3 @@
 macports_package "npm" do
   action :install
 end
-
-execute "install n version manager for node.js" do
-  command "npm install -g n"
-  not_if "npm list -g | grep ' n@'"
-end
-
-workstation_bash_profile_include "node_js"
-
-execute "make directory to hold installations of node via n version manager" do
-  command "mkdir #{ENV['HOME']}/.n"
-  not_if File.directory? "#{ENV['HOME']}/.n"
-  user WS_USER
-end
