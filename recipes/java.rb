ac60fb1e9e1c94080edb4a18c6708353e75b528e
Brandon Valentine <brandon@brandonvalentine.com>
2012-09-09 15:46:27 -0500

    Update Java for OS X recipe

--- recipes/java.rb
+++ recipes/java.rb
@@ -1,8 +1,8 @@
 workstation_package "JavaForOSX" do
-  source "http://support.apple.com/downloads/DL1515/en_US/"
-  volumes_dir "Java for OS X 2012-004"
+  source "http://support.apple.com/downloads/DL1572/en_US/"
+  volumes_dir "Java for OS X 2012-005"
   action :install
   type "pkg"
   package_id "com.apple.pkg.JavaForMacOSX107"
-  checksum "817ea23add35a71b9b217a05eeb7862cd3b815246b1bfca35ba54480181362fd"
+  checksum "7c878d0830d9c51385bf87642fca641b624f3eb5e5b41e039bc1bf86cb50c1e4"
 end
