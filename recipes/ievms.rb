e568a66105e5240c1429b707fca27dc771e09bca
Brandon Valentine <brandon@brandonvalentine.com>
2012-11-05 17:25:28 -0600

    Remove marker file wrapper for ievms

--- recipes/ievms.rb
+++ recipes/ievms.rb
@@ -8,11 +8,7 @@ macports_package "curl" do
   action :install
 end
 
-run_unless_marker_file_exists(marker_version_string_for("ievms")) do
-
-  execute "install ievms" do
-    command "curl -s https://raw.github.com/xdissent/ievms/master/ievms.sh | bash"
-    user WS_USER
-  end
-
+execute "install ievms" do
+  command "curl -s https://raw.github.com/xdissent/ievms/master/ievms.sh | bash"
+  user WS_USER
 end
