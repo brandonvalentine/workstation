d77669f325a0d4dad56d9860a606605b57aefa41
Brandon Valentine <brandon@brandonvalentine.com>
2013-01-02 11:22:33 -0600

    Add calibre

---
+++ recipes/calibre.rb
@@ -0,0 +1,7 @@
+workstation_package "calibre" do
+  dmg_name "calibre-0.9.12"
+  volumes_dir "calibre-0.9.12"
+  source "http://calibre-ebook.googlecode.com/files/calibre-0.9.12.dmg"
+  checksum "92eb3956565e8c1de74f1ba4f3fcb1138ffcfe573c0ae4600dd19b4eede853c8"
+  action :install
+end
